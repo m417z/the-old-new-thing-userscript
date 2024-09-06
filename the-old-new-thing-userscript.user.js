65cef8e7b163e8e915e74892d1b6c8cc9b6eec50
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2024-09-06 21:41:48 +0300

    Add license

--- the-old-new-thing-userscript.user.js
+++ the-old-new-thing-userscript.user.js
@@ -7,6 +7,7 @@
 // @match        https://devblogs.microsoft.com/oldnewthing/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
 // @grant        none
+// @license      MIT
 // @noframes
 // ==/UserScript==
 
